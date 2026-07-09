31dcf7080b5eacbe2b33f953f7127c5009a88e01
Krrishpana Karmacharya <krrishpana@gmail.com>
2026-07-09 21:39:22 +0545

    Time: 31 ms (21.8%), Space: 25.9 MB (59.12%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        seen = set()
+
+        for i in nums:
+            if i not in seen:
+                seen.add(i)
+            else:
+                return True
+        return False
+    nums = [1,2,3,1]
+        
